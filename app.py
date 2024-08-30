8b633edb0c8f3118632e4918aa3afa47dae6bf1c
Santiago Ramirez Forero <saramirez@mindeporte.gov.co>
2024-08-30 16:46:44 -0500

    cambios

--- app.py
+++ app.py
@@ -3,4 +3,3 @@ from app import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Usa el puerto asignado por el entorno, con un valor por defecto de 8080
-    app.run(host='0.0.0.0', port=port, debug=False)
