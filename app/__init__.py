d2bd58ad25d42382aea4827ed0639d875fb7b67d
Santiago Ramirez Forero <saramirez@mindeporte.gov.co>
2024-08-31 09:25:39 -0500

    cambios

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,12 @@ app.config['JWT_SECRET_KEY'] = secret_key
 db = SQLAlchemy(app)
 
 # Configure CORS to allow multiple origins
-CORS(app, resources={r"/*": {"origins": "*"}})
+allowed_origins = [
+    'http://localhost:3000',
+    'https://mintickets.vercel.app'
+]
+
+CORS(app, resources={r"/*": {"origins": allowed_origins, "supports_credentials": True}})
 
 jwt = JWTManager(app)
 
