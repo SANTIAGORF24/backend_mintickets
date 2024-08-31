7afdf4210bb1f9b29fb26acf6638719437d0cafb
Santiago Ramirez Forero <saramirez@mindeporte.gov.co>
2024-08-30 23:14:23 -0500

    cambios

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-# Aquí deberías importar tus rutas y configuraciones
-# from .routes import ...
-# from .config import ...
+from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
