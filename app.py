493e007e1c5e236ec18cb55b28089b27bb9fa5ce
Santiago Ramirez Forero <saramirez@mindeporte.gov.co>
2024-08-30 23:44:51 -0500

    cambios

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from app import app
+from waitress import serve
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
+    serve(app, host='0.0.0.0', port=port)
