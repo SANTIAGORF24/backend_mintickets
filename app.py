2ba7b59c08853914028912bcc58b2274be321595
Santiago Ramirez Forero <saramirez@mindeporte.gov.co>
2024-09-02 12:46:38 -0500

    vercel

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
+from app.routes.auth_routes import bp as auth_bp
 
 app = Flask(__name__)
+
+# Registra el blueprint de autenticación
+app.register_blueprint(auth_bp)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-# Add this for Vercel
-app = app
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
